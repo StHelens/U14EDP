2c289dcb035727eab6a03bfaac34a8685cb312ca
Gary Davies <garydavies@outlook.com>
2015-09-15 00:44:56 +0100

    Initial commit

---
+++ HelloWPF1/HelloWPF1.py
@@ -0,0 +1,14 @@
+﻿import wpf
+
+from System.Windows import Application, Window
+
+class MyWindow(Window):
+    def __init__(self):
+        wpf.LoadComponent(self, 'HelloWPF1.xaml')
+    
+    def button_Click(self, sender, e):
+        self.label.Content = "Hello WPF"
+    
+
+if __name__ == '__main__':
+    Application().Run(MyWindow())
---
+++ WPF2Input/WPF2Input.py
@@ -0,0 +1,20 @@
+﻿import wpf
+
+from System.Windows import Application, Window
+
+class MyWindow(Window):
+    def __init__(self):
+        wpf.LoadComponent(self, 'WPF2Input.xaml')  
+        self.label.Content = "Enter your name"
+        self.textBox.Text = ""
+
+    def inputName(self):
+        userName = self.textBox.Text
+        return userName
+    
+    def button_Click(self, sender, e):
+        userName = self.inputName()
+        self.label.Content = ("Welcome " + userName.capitalize() + " it's nice to meet you")
+    
+if __name__ == '__main__':
+    Application().Run(MyWindow())
