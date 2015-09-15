923f46308275d6f572e2d91ffa7968cbdda2eedc
Gary Davies <garydavies@outlook.com>
2015-09-15 16:08:30 +0100

    Wpf1Buttons added

---
+++ Wpf1Buttons/Wpf1Buttons.py
@@ -0,0 +1,23 @@
+﻿import wpf
+
+from System.Windows import Application, Window
+
+class MyWindow(Window):
+    def __init__(self):
+        wpf.LoadComponent(self, 'Wpf1Buttons.xaml')
+        self.message = "Group %s is the best"
+    
+    def Group2_Click(self, sender, e):
+        self.label.Content = self.message % 2
+    
+    def Group3_Click(self, sender, e):
+        self.label.Content = self.message % 3
+    
+    def Group4_Click(self, sender, e):
+        self.label.Content = self.message % 4
+    
+    def Group5_Click(self, sender, e):
+        self.label.Content = self.message % 5
+
+if __name__ == '__main__':
+    Application().Run(MyWindow())
