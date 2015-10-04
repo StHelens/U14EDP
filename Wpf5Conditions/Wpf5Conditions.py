a504f2a9418ab1adacc6e84779ec558246f31fe1
Gary Davies <garydavies@outlook.com>
2015-10-04 22:14:57 +0100

    Wpf5Conditions

---
+++ Wpf5Conditions/Wpf5Conditions.py
@@ -0,0 +1,44 @@
+import wpf
+
+from System.Windows import Application, Window
+
+class MyWindow(Window):
+    def __init__(self):
+        wpf.LoadComponent(self, 'Wpf5Conditions.xaml')
+        self.battlefront = 43.99
+        self.halo = 41.99
+        self.fifa = 44.99
+        self.subtotal = 0
+        self.label.Content = "Add items to basket below "
+        self.label1.Content = ("Battlefront = %s" % self.battlefront)
+        self.label2.Content = ("Halo 5 = %s" % self.halo)
+        self.label3.Content = ("FIFA 16 = %s" % self.fifa)
+        self.label4.Content = "Total"
+        self.button.Content = "Calculate"
+    
+    def button_Click(self, sender, e):
+        if self.subtotal < 50:
+            self.label4.Content = ("Total = {0:.2f}".format(self.subtotal + 10))
+        else:
+            self.label4.Content = ("Total = {0:.2f}".format(self.subtotal))
+    
+    def checkBox_Checked(self, sender, e):
+        self.subtotal = self.subtotal + self.battlefront    
+
+    def checkBox_Unchecked(self, sender, e):
+        self.subtotal = self.subtotal - self.battlefront
+
+    def checkBox1_Checked(self, sender, e):
+        self.subtotal = self.subtotal + self.halo 
+
+    def checkBox1_Unchecked(self, sender, e):
+        self.subtotal = self.subtotal - self.halo
+    
+    def checkBox2_Checked(self, sender, e):
+        self.subtotal = self.subtotal + self.fifa
+
+    def checkBox2_Unchecked(self, sender, e):
+        self.subtotal = self.subtotal - self.fifa
+
+if __name__ == '__main__':
+    Application().Run(MyWindow())
